4fe580f7340a4d679ed622686ce302611d5e0d51
Yinan Li <gabriel.li5566@gmail.com>
2022-07-05 15:44:55 +0300

    completed update password function

--- router/userinfo.js
+++ router/userinfo.js
@@ -1,17 +1,26 @@
 const express = require('express')
 const router = express.Router()
 const expressJoi = require('@escook/express-joi')
-const { update_userinfo_schema } = require('../schema/user')
+const {
+  update_userinfo_schema,
+  update_password_schema,
+} = require('../schema/user')
 
 // 导入路由处理函数模块
 const userinfo_handler = require('../router_handler/userinfo')
-// 获取用户的基本信息
+// 获取用户的基本信息的路由
 router.get('/userinfo', userinfo_handler.getUserinfo)
-// 更新用户的基本信息
+// 更新用户的基本信息的路由
 router.post(
   '/userinfo',
   expressJoi(update_userinfo_schema),
   userinfo_handler.updateUserinfo
+)
+// 重置用户密码的路由
+router.post(
+  '/updatepwd',
+  expressJoi(update_password_schema),
+  userinfo_handler.updatePassword
 )
 
 module.exports = router
--- router_handler/userinfo.js
+++ router_handler/userinfo.js
@@ -1,4 +1,7 @@
 const db = require('../db/index')
+const bcrypt = require('bcryptjs')
+const { login } = require('./user')
+const { response } = require('express')
 // 获取用户信息处理模块
 //! 注意：为了防止用户的密码泄露，需要排除 password 字段
 exports.getUserinfo = (req, res) => {
@@ -29,5 +32,34 @@ exports.updateUserinfo = (req, res) => {
       return res.cc('未找到该用户，更新用户信息失败！')
     // 3. 将修改成功的信息响应给客户端
     return res.cc('更新用户基本信息成功')
+  })
+}
+
+exports.updatePassword = (req, res) => {
+  const sqlStr = 'select * from ev_users where id = ?'
+  // 执行 SQL 语句查询用户是否存在
+  db.query(sqlStr, [req.auth.id], (err, results) => {
+    // 1. 执行 SQL 语句失败
+    if (err) return res.cc(err)
+    // 2. 执行 SQL 语句成功，但是查询到的数据条数不等于 1，未找到该用户。
+    if (results.length !== 1) return res.cc('未找到该用户，密码重置失效！')
+    // 3. 判断原密码是否正确
+    const compareResult = bcrypt.compareSync(
+      req.body.oldPwd,
+      results[0].password
+    )
+    if (!compareResult) return res.cc('原密码验证失败')
+
+    const sqlStr1 = 'update ev_users set password = ? where id = ?'
+    req.body.newPwd = bcrypt.hashSync(req.body.newPwd, 10)
+    db.query(sqlStr1, [req.body.newPwd, req.auth.id], (err, results) => {
+      // 1. 执行 SQL 语句失败
+      if (err) return res.cc(err)
+      // 2. 执行 SQL 语句成功，但是查询到的数据条数不等于 1。
+      if (results.affectedRows !== 1)
+        return res.cc('未找到该用户，更新用户信息失败！')
+      // 3. 将密码修改成功的信息响应给客户端
+      return res.cc('密码修改成功')
+    })
   })
 }
